0dd88d312859c2903b73c361dfeadd0bd7025b43
anand tummapudi <anand.tummapudi@gmail.com>
2023-03-30 08:09:42 +0530

    Array Implementation

--- DataStructures/src/com/datastructures/arrays/Array.java
+++ DataStructures/src/com/datastructures/arrays/Array.java
@@ -1,5 +1,51 @@
 package com.datastructures.arrays;
 
 public class Array {
+	private int [] items;
+	private int count=0;
+	
+	public Array(int size){
+		items = new int[size];
+	}
+	
+	public void print() {
+		for (int i=0;i<count;i++) {
+			System.out.println(items[i]);
+		}
+	}
+	
+	public void add(int item) {
+		if(items.length==count) {
+			int[] newItems = new int[count*2];
+			for(int i=0;i<count;i++) {
+				newItems[i] = items[i];
+			}
+			
+			items = newItems;
+		}
+		items[count++] = item;
+		
+	} 
+	
+	public void remove(int index) {
+		
+		if(index<0 || index>=count) {
+			throw new IllegalArgumentException("Illegal Argument");
+		}
+		
+		for(int i=index;i<count;i++) {
+			items[i] = items[i+1];
+		}
+		
+		count--;
+	}
+	
+	public int indexOf(int item) {
+		for(int i=0;i<count;i++) {
+			if(items[i]==item)
+				return i;
+		}
+		return -1;
+	}
 
 }
--- DataStructures/src/com/datastructures/arrays/ArrayImpl.java
+++ DataStructures/src/com/datastructures/arrays/ArrayImpl.java
@@ -1,5 +1,25 @@
 package com.datastructures.arrays;
 
 public class ArrayImpl {
-
+	
+	public static void main(String []args) {
+		Array numbers = new Array(3);
+		numbers.add(10);
+		numbers.add(20);
+		numbers.add(30);
+		numbers.add(40);
+		numbers.add(50);
+		numbers.print();
+		
+		System.out.println("Remove ELement");
+		
+		//numbers.remove(2);
+		//numbers.print();
+		
+		System.out.println("Index of an element");
+		int index = numbers.indexOf(40);
+		System.out.println(index);
+		
+	}
+	
 }
